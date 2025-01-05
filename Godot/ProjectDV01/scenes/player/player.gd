2cad221218bf3cd2277d45569b98e0f4b009071b
StudiesGitHub <ncsaraf@outlook.com>
2025-01-04 21:29:29 -0300

    update

--- Godot/ProjectDV01/scenes/player/player.gd
+++ Godot/ProjectDV01/scenes/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var movement = player_movement()
+	var direction = movement.normalized()
+	
+	velocity = direction * MAX_SPEED
+	
+	move_and_slide()
+
+func player_movement():
+	var x_movement = Input.get_action_strength("move_left") - Input.get_action_strength("move_right")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement)
+	

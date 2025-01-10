e8f23f8a3a761f1ef86bc2508f34d3998c1a0809
StudiesGitHub <ncsaraf@outlook.com>
2025-01-10 07:24:49 -0300

    update

--- Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Card.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Card.tsx
@@ -1,8 +1,11 @@
 export function Card() {
   return (
     <div className="h-20 w-full rounded p-4 flex items-center justify-center gap-x-4 bg-gray500 text-gray100">
-      <input type="radio" />
-      <p>
+      <div
+        id="circle"
+        className="bg-red-500 h-4 w-4 rounded-full flex-shrink-0 border-s-white-2"
+      />
+      <p className="flex-grow">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius atque
         suscipit beatae repellat eum tenetur error nihil blanditiis! Provident,
         explicabo laborum alias repellendus numquam quaerat magni ducimus qui

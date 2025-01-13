fe225a14bcc745e7f5b5bcc435690362c469c9e0
StudiesGitHub <ncsaraf@outlook.com>
2025-01-13 19:50:58 -0300

    update

--- Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Card.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Card.tsx
@@ -1,10 +1,26 @@
+import { Check } from "phosphor-react";
+import { useState } from "react";
+
 export function Card() {
+  const [isChecked, setisChecked] = useState(true);
+
   return (
     <div className="h-20 w-full rounded p-4 flex items-center justify-center gap-x-4 bg-gray500 text-gray100">
-      <div
-        id="circle"
-        className="bg-red-500 h-4 w-4 rounded-full flex-shrink-0 border-s-white-2"
-      />
+      {!isChecked ? (
+        <div
+          id="circle"
+          className="h-6 w-6 rounded-full flex-shrink-0 border-blue-400 border-solid border-2 "
+          onClick={() => setisChecked(true)}
+        />
+      ) : (
+        <div
+          id="circle"
+          className="h-6 w-6 rounded-full flex-shrink-0 bg-purpleDark flex items-center justify-center"
+          onClick={() => setisChecked(false)}
+        >
+          <Check size={16} />
+        </div>
+      )}
       <p className="flex-grow">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius atque
         suscipit beatae repellat eum tenetur error nihil blanditiis! Provident,

b7c4f4bf6eacd6ed924809e0724ed1be86534e79
StudiesGitHub <ncsaraf@outlook.com>
2025-01-13 20:29:30 -0300

    update

--- Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Cards/components/CheckCircle.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Body/components/Cards/components/CheckCircle.tsx
@@ -4,17 +4,21 @@ import { useState } from "react";
 export default function CheckCircle() {
   const [isChecked, setisChecked] = useState(false);
 
+  function handleCheck() {
+    setisChecked((state) => !state);
+  }
+
   return !isChecked ? (
     <div
       id="circle"
       className="h-6 w-6 rounded-full flex-shrink-0 border-blue-400 border-solid border-2 "
-      onClick={() => setisChecked(true)}
+      onClick={handleCheck}
     />
   ) : (
     <div
       id="circle"
       className="h-6 w-6 rounded-full flex-shrink-0 bg-purpleDark flex items-center justify-center"
-      onClick={() => setisChecked(false)}
+      onClick={handleCheck}
     >
       <Check size={16} />
     </div>
--- Ignite-ReactJS/Desafios/desafio01/src/components/Header/components/Input.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Header/components/Input.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 export function Input() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  function handleCreateTask() {
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
+  console.log(tasks);
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTask(event.target.value);
+  }
   return (
     <div className="flex gap-x-4 relative -bottom-4">
       <input
         type="text"
         placeholder="Adicione uma nova tarefa"
         className="bg-gray500 h-8 rounded w-80 px-2 text-justify"
+        onChange={handleInputChange}
       />
-      <button className="bg-blueDark rounded w-20 h-8 text-white">Criar</button>
+      <button
+        className="bg-blueDark rounded w-20 h-8 text-white"
+        onClick={handleCreateTask}
+      >
+        Criar
+      </button>
     </div>
   );
 }

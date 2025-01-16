7296c0a07bbba55a664b2d51e1fee422c5bf89d5
StudiesGitHub <ncsaraf@outlook.com>
2025-01-16 10:15:26 -0300

    update

--- Ignite-ReactJS/Desafios/desafio01/src/components/Body/Body.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Body/Body.tsx
@@ -6,15 +6,15 @@ export function Body({ tasks }: { tasks: string[] }) {
 
   return (
     <body className="bg-gray600 h-screen flex flex-col items-center pt-16">
-      <section className="flex w-1/3 justify-between">
+      <section className="flex w-1/3 justify-between p-2">
         <div className="flex gap-2">
-          <p className="text-blue-400 bold">Tarefas criadas</p>
+          <p className="text-blue-300 bold">Tarefas criadas</p>
           <div className="bg-gray500 rounded-full text-gray-100 px-3 justify-center flex">
             5
           </div>
         </div>
         <div className="flex gap-2">
-          <p className="text-purple-500">Concluídas</p>
+          <p className="text-purple-400">Concluídas</p>
           <div className="bg-gray500 rounded-full text-gray-100 px-3">
             2 de 5
           </div>
@@ -28,7 +28,7 @@ export function Body({ tasks }: { tasks: string[] }) {
             ))}
           </div>
         ) : (
-          <div className="flex flex-col items-center text-gray-400 pt-4 border-t-2 border-solid border-gray-500  w-1/3">
+          <div className="flex flex-col items-center text-gray-400 pt-4 border-t-2 border-solid border-gray-500">
             <Note size={64} />
             <p className="bold text-lg">
               Você ainda não tem tarefas cadastradas

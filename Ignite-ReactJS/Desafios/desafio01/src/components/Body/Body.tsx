f45cf8c117a2ea81b0211ee897b923f670463352
StudiesGitHub <ncsaraf@outlook.com>
2025-01-16 10:13:07 -0300

    update

--- Ignite-ReactJS/Desafios/desafio01/src/components/Body/Body.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Body/Body.tsx
@@ -5,20 +5,38 @@ export function Body({ tasks }: { tasks: string[] }) {
   console.log(tasks.length);
 
   return (
-    <body className="bg-gray600 h-screen flex justify-center pt-16">
-      {tasks.length !== 0 ? (
-        <section className="h-2/3 w-1/3 border-t-2 border-solid border-gray-500 pt-4 gap-y-2 flex flex-col">
-          {tasks.map((task) => (
-            <Card task={task} />
-          ))}
-        </section>
-      ) : (
-        <section className="flex flex-col items-center text-gray-400 pt-4 border-t-2 border-solid border-gray-500  w-1/3">
-          <Note size={64} />
-          <p className="bold text-lg">Você ainda não tem tarefas cadastradas</p>
-          <p className="text-g">Crie tarefas e organize seus itens a fazer</p>
-        </section>
-      )}
+    <body className="bg-gray600 h-screen flex flex-col items-center pt-16">
+      <section className="flex w-1/3 justify-between">
+        <div className="flex gap-2">
+          <p className="text-blue-400 bold">Tarefas criadas</p>
+          <div className="bg-gray500 rounded-full text-gray-100 px-3 justify-center flex">
+            5
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <p className="text-purple-500">Concluídas</p>
+          <div className="bg-gray500 rounded-full text-gray-100 px-3">
+            2 de 5
+          </div>
+        </div>
+      </section>
+      <section className="h-2/3 w-1/3 ">
+        {tasks.length !== 0 ? (
+          <div className="border-t-2 border-solid border-gray-500 pt-4 gap-y-2 flex flex-col">
+            {tasks.map((task) => (
+              <Card task={task} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center text-gray-400 pt-4 border-t-2 border-solid border-gray-500  w-1/3">
+            <Note size={64} />
+            <p className="bold text-lg">
+              Você ainda não tem tarefas cadastradas
+            </p>
+            <p className="text-g">Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        )}
+      </section>
     </body>
   );
 }

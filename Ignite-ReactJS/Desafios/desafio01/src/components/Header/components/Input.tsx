ec67e2b7a188f0e0a86d5b068eb993df8a45ab64
StudiesGitHub <ncsaraf@outlook.com>
2025-01-16 08:41:27 -0300

    up

--- Ignite-ReactJS/Desafios/desafio01/src/components/Header/components/Input.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/components/Header/components/Input.tsx
@@ -6,9 +6,17 @@ interface InputProps {
 }
 
 export function Input({ tasks, setTasks }: InputProps) {
+  const [error, setError] = useState("");
   const [task, setTask] = useState("");
 
   function handleCreateTask() {
+    if (task.trim() === "") {
+      setError("O campo nao pode ficar vazio");
+
+      return;
+    }
+
+    setError("");
     setTasks([...tasks, task]);
     setTask("");
   }
@@ -17,19 +25,22 @@ export function Input({ tasks, setTasks }: InputProps) {
     setTask(event.target.value);
   }
   return (
-    <div className="flex gap-x-4 relative -bottom-4">
-      <input
-        type="text"
-        placeholder="Adicione uma nova tarefa"
-        className="bg-gray500 h-8 rounded w-80 px-2 text-justify text-gray-100 placeholder:text-gray-400"
-        onChange={handleInputChange}
-      />
-      <button
-        className="bg-blueDark rounded w-20 h-8 text-white"
-        onClick={handleCreateTask}
-      >
-        Criar
-      </button>
+    <div>
+      <div className="flex gap-x-4 relative -bottom-4">
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          className="bg-gray500 h-8 rounded w-80 px-2 text-justify text-gray-100 placeholder:text-gray-400"
+          onChange={handleInputChange}
+        />
+        <button
+          className="bg-blueDark rounded w-20 h-8 text-white"
+          onClick={handleCreateTask}
+        >
+          Criar
+        </button>
+      </div>
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 }

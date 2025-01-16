6d3504b212daa7fc0841b6e34df95b5b402eef61
StudiesGitHub <ncsaraf@outlook.com>
2025-01-16 10:44:23 -0300

    up

--- Ignite-ReactJS/Desafios/desafio01/src/App.tsx
+++ Ignite-ReactJS/Desafios/desafio01/src/App.tsx
@@ -5,6 +5,10 @@ import { Header } from "./components/Header/Header";
 function App() {
   const [tasks, setTasks] = useState<string[]>([]);
 
+  function handleDelete(event: React.MouseEvent<HTMLButtonElement>) {
+    tasks.filter((task) => task);
+  }
+
   return (
     <div>
       <Header tasks={tasks} setTasks={setTasks} />

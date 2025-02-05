6f883e65490e3937b0cd9ba51827a5e848af1107
StudiesGitHub <ncsaraf@outlook.com>
2025-02-04 21:44:47 -0300

    up

--- Ignite-ReactJS/Desafios/desafio02/src/components/Body/Article/index.tsx
+++ Ignite-ReactJS/Desafios/desafio02/src/components/Body/Article/index.tsx
@@ -1,16 +1,10 @@
 import Panel from "../../../imgs/Panel.png";
-import { InfoTag } from "./InfoTag";
+import { Info } from "./Info";
 
 export function Article() {
   return (
-    <article className="bg-gray-100 text-gray-900">
-      <p className="text-2xl font-extrabold">
-        Encontre o café perfeito para qualquer hora do dia
-      </p>
-      <h2>
-        Com o Coffe Delivery você recebe seu café onde estiver, a qualquer hora
-      </h2>
-      <InfoTag />
+    <article className="bg-gray-100 text-gray-900 flex justify-between">
+      <Info />
       <img src={Panel} className="flex-shrink-0" />
     </article>
   );

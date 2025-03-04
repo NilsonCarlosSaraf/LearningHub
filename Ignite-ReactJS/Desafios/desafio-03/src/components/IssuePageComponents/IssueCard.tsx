b96bd39051fd7d675fdcfe49581ac3b49d3108f1
StudiesGitHub <ncsaraf@outlook.com>
2025-03-04 20:38:32 -0300

    update

--- Ignite-ReactJS/Desafios/desafio-03/src/components/IssuePageComponents/IssueCard.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/components/IssuePageComponents/IssueCard.tsx
@@ -1,6 +1,6 @@
 export function IssueCard() {
   return (
-    <div className="w-[26rem] h-[16.25rem] flex flex-col gap-4 p-4 rounded bg-[#112131]">
+    <div className="w-[54rem] h-[10.5rem] flex flex-col gap-4 p-4 rounded bg-[#112131] absolute -top-[5.25rem]">
       <h1 className="font-bold text-baseTitle">
         JavaScript data types and data structures
       </h1>
--- Ignite-ReactJS/Desafios/desafio-03/src/pages/IssuePage.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/pages/IssuePage.tsx
@@ -2,7 +2,7 @@ import { IssueCard } from "../components/IssuePageComponents/IssueCard";
 
 export function IssuePage() {
   return (
-    <div className="flex flex-col justify-center items-center pt-[10rem] w-full bg-[#071422]">
+    <div className="flex flex-col justify-center items-center pt-[10rem] w-full bg-[#071422] relative">
       <IssueCard />
       IssuePage
     </div>

f148c9a2c3c39c209518ceacd8c382d0fac88201
StudiesGitHub <ncsaraf@outlook.com>
2025-03-04 14:14:51 -0300

    finish proejct structure

--- Ignite-ReactJS/Desafios/desafio-03/src/components/Body/SearchBar.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/components/Body/SearchBar.tsx
@@ -1,6 +1,6 @@
 export function SearchBar() {
   return (
-    <div className="flex flex-col w-full p-4 gap-2">
+    <div className="flex flex-col w-full p-y-4 pb-4 gap-2">
       <div className="flex justify-between items-center">
         <h1 className="text-baseSubtitle">Publicações</h1>
         <span className="text-baseSpan">6 publicações</span>

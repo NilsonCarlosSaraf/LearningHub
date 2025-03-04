b32691887dae94fe90136a9395c7ecd9c73cdc26
StudiesGitHub <ncsaraf@outlook.com>
2025-03-04 14:08:55 -0300

    including all colors to project

--- Ignite-ReactJS/Desafios/desafio-03/src/components/Body/SearchBar.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/components/Body/SearchBar.tsx
@@ -2,13 +2,13 @@ export function SearchBar() {
   return (
     <div className="flex flex-col w-full p-4">
       <div className="flex justify-between items-center">
-        <h1>Publicacoes</h1>
-        <span>6 publicaçoes</span>
+        <h1 className="text-baseSubtitle">Publicacoes</h1>
+        <span className="text-baseSpan">6 publicaçoes</span>
       </div>
       <input
         type="text"
         placeholder="Buscar conteúdo"
-        className="bg-[#040F1A]"
+        className="bg-[#040F1A] placeholder:text-baseLabel"
       />
     </div>
   );

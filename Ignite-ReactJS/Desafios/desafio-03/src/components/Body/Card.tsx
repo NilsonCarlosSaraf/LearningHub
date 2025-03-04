0e37b76dbb37850de02835470b5fca2cce5f970a
StudiesGitHub <ncsaraf@outlook.com>
2025-03-04 14:16:58 -0300

    ^update^

--- Ignite-ReactJS/Desafios/desafio-03/src/components/Body/Card.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/components/Body/Card.tsx
@@ -1,9 +1,12 @@
 export function Card() {
   return (
     <div className="w-[26rem] h-[16.25rem] flex flex-col gap-4 p-4 rounded bg-[#112131]">
-      <h1 className="font-bold text-baseTitle">
-        JavaScript data types and data strctures
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="font-bold text-baseTitle">
+          JavaScript data types and data strctures
+        </h1>
+        <span className="text-baseSpan">Há 1 dia</span>
+      </div>
       <p className="text-baseText">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
         deserunt, a tempora consequatur, et ullam voluptates accusantium

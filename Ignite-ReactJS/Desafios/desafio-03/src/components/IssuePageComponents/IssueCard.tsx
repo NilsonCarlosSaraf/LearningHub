e05ed2c6b3837e34e1e031d7d5671fee0b32e8c8
StudiesGitHub <ncsaraf@outlook.com>
2025-03-05 21:55:35 -0300

    update

--- Ignite-ReactJS/Desafios/desafio-03/src/components/IssuePageComponents/IssueCard.tsx
+++ Ignite-ReactJS/Desafios/desafio-03/src/components/IssuePageComponents/IssueCard.tsx
@@ -1,6 +1,10 @@
 export function IssueCard() {
   return (
     <div className="w-[54rem] h-[10.5rem] flex flex-col gap-4 p-4 rounded bg-[#112131] absolute -top-[5.25rem]">
+      <div className="flex justify-between">
+        <button className="text-[#3294F8]">VOLTAR</button>
+        <button className="text-[#3294F8]">VER NO GITHUB</button>
+      </div>
       <h1 className="font-bold text-baseTitle">
         JavaScript data types and data structures
       </h1>

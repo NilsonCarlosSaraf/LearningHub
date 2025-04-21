328dc6f09aef83f254a7be1aade59a42cd16e429
StudiesGitHub <ncsaraf@outlook.com>
2025-04-21 18:33:55 -0300

    update

---
+++ c#/modulo3/MyFirstApi/MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Cache;
+
+namespace MyFirstApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult GetById([FromQuery]int id, [FromQuery] string? nickname)
+    {
+        var response = new User
+        {
+            Id = 1,
+            Age = 7,
+            Name = "Nilson"
+        };
+
+        return Ok(response);
+    }
+}

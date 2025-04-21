b09a0366ea92f400d3d15dc6c6ca9939765cad96
StudiesGitHub <ncsaraf@outlook.com>
2025-04-20 21:25:19 -0300

    update

---
+++ c#/modulo1/Desafio01/Desafio01/Desafio01/Classes/FormatadorDeData.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace Desafio01.Classes;
+
+internal class FormatadorDeData
+{
+    public static void FormatarData()
+    {
+        DateTime agora = DateTime.Now;
+        CultureInfo formatoDeDataBr = new CultureInfo("pt-BR");
+
+        Console.WriteLine("Escolha uma das opções para exibir a data/hora:");
+        Console.WriteLine("1 - Formato completo");
+        Console.WriteLine("2 - Apenas a data (dd/MM/yyyy)");
+        Console.WriteLine("3 - Apenas a hora (HH:mm:ss)");
+        Console.WriteLine("4 - Data com mês por extenso");
+        Console.Write("Opção: ");
+        string opcao = Console.ReadLine();
+
+        Console.WriteLine();
+
+        switch (opcao)
+        {
+            case "1":
+                Console.WriteLine("Formato completo:");
+                Console.WriteLine(agora.ToString("dddd, dd 'de' MMMM 'de' yyyy HH:mm:ss", formatoDeDataBr));
+                break;
+
+            case "2":
+                Console.WriteLine("Apenas a data:");
+                Console.WriteLine(agora.ToString("dd/MM/yyyy"));
+                break;
+
+            case "3":
+                Console.WriteLine("Apenas a hora:");
+                Console.WriteLine(agora.ToString("HH:mm:ss"));
+                break;
+
+            case "4":
+                Console.WriteLine("Data com mês por extenso:");
+                Console.WriteLine(agora.ToString("dd 'de' MMMM 'de' yyyy", formatoDeDataBr));
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
+    }
+}
--- c#/modulo1/Desafio01/Desafio01/Desafio01/Program.cs
+++ c#/modulo1/Desafio01/Desafio01/Desafio01/Program.cs
@@ -5,25 +5,27 @@ class Program
 {
     static void Main()
     {
-        ContadorDeCaracteres contador = new ContadorDeCaracteres();
+        //ContadorDeCaracteres contador = new ContadorDeCaracteres();
 
-        InsertName.ReceiveName();
-        Operacoes.ReceberValores();
-        Operacoes.SomarValores();
-        Operacoes.SubtrairValores();
-        Operacoes.MultiplicarValores();
-        Operacoes.DividirValores();
+        //InsertName.ReceiveName();
+        //Operacoes.ReceberValores();
+        //Operacoes.SomarValores();
+        //Operacoes.SubtrairValores();
+        //Operacoes.MultiplicarValores();
+        //Operacoes.DividirValores();
 
-        contador.ReceberCaracteres();
-        contador.ContarCaracteres();
+        //contador.ReceberCaracteres();
+        //contador.ContarCaracteres();
 
 
-        Console.Write("Digite a placa do veículo: ");
-        string? placa = Console.ReadLine();
+        //Console.Write("Digite a placa do veículo: ");
+        //string? placa = Console.ReadLine();
 
-        bool valida = ValidadorDePlaca.PlacaValida(placa);
+        //bool valida = ValidadorDePlaca.PlacaValida(placa);
 
-        Console.WriteLine(valida ? "Placa valida" : "Placa invalida");
+        //Console.WriteLine(valida ? "Placa valida" : "Placa invalida");
+
+        FormatadorDeData.FormatarData();
 
     }
 }

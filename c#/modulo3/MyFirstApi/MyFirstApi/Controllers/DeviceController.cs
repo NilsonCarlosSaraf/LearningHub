e60f10267ed63474623a7a881e9ea951630286bf
StudiesGitHub <ncsaraf@outlook.com>
2025-04-25 16:19:33 -0300

    update

---
+++ c#/modulo3/MyFirstApi/MyFirstApi/Controllers/DeviceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+public class DeviceController : MyFirstApiBaseController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        Author = "Joaos";
+
+        return Ok(Author);
+    }
+
+}

f9984bdc6d332a1a6233adafea7b4783fb5ec504
StudiesGitHub <ncsaraf@outlook.com>
2025-05-04 20:39:15 -0300

    adding first controller

---
+++ c#/modulo4/Petfolio/Petfolio.Communication/Requests/RequestRegisteredPetJson.cs
@@ -0,0 +1,9 @@
+﻿using Petfolio.Communication.Enums;
+namespace Petfolio.Communication.Requests;
+
+public class RequestRegisteredPetJson
+{
+    public string Name { get; set; } = string.Empty;
+    public DateTime BirthDay { get; set; }
+    public PetType Type { get; set; }
+}

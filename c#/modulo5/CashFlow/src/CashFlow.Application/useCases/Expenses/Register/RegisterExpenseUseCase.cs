4c24baac6d09d76cb62553c39768eba9ae62f6c9
StudiesGitHub <ncsaraf@outlook.com>
2025-05-13 13:23:08 -0300

    add first usecase

---
+++ c#/modulo5/CashFlow/src/CashFlow.Application/useCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,12 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.useCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        return new ResponseRegisteredExpenseJson();
+    }
+}

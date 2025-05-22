d3c0f0f0b0083c0a29c4be0fd3f64cbabfab59a6
StudiesGitHub <ncsaraf@outlook.com>
2025-05-22 10:05:28 -0300

    create test

---
+++ c#/modulo5/CashFlow/tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.useCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Test.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Apple",
+            Amount = 100,
+            Description="Description",
+            Date = DateTime.Now.AddDays(-1),
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+        //Act;
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}

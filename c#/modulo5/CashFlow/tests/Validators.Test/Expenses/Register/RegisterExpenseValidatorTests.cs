f022a19ce24db5940a50cbfe88ca3f0cd02bca44
StudiesGitHub <sarafnilson@gmail.com>
2025-05-25 16:37:44 -0300

    include fluentassertions package to project

--- c#/modulo5/CashFlow/tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ c#/modulo5/CashFlow/tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,6 @@
 ﻿using CashFlow.Application.useCases.Expenses.Register;
-using CashFlow.Communication.Requests;
 using CommonTestUtilities.Requests;
+using FluentAssertions;
 
 namespace Validators.Test.Expenses.Register;
 
@@ -16,6 +16,6 @@ public class RegisterExpenseValidatorTests
         var result = validator.Validate(request);
 
         //Assert
-        Assert.True(result.IsValid);
+        result.IsValid.Should().BeTrue();
     }
 }

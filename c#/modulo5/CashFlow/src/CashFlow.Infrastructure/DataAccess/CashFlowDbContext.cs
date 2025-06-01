29b87721e6ba69bfb4fb005985ca9e0299ef19d4
StudiesGitHub <sarafnilson@gmail.com>
2025-06-01 16:35:07 -0300

    add context

---
+++ c#/modulo5/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "";
+
+        var version = new Version(8, 0, 42);
+        var serverVersion = new MySqlServerVersion(version); // Adjust version as needed
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}

509785ef206154fe4dd63388812bce4f15eee5ae
StudiesGitHub <sarafnilson@gmail.com>
2025-06-19 10:17:22 -0300

    basic set up

---
+++ nodejs/express/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// rota teste
+app.get('/', (req, res) => {
+  res.send('API com Express funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
